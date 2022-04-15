8b5e79377d7d1a69275f080b2422d9ffeee59c5d
dyatkokg <dyatkokg@gmail.com>
2022-04-15 21:13:47 +0400

    first commit

---
+++ src/main/java/me/dyatkokg/bookreaderusersapi/BookReaderUsersApiApplication.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.bookreaderusersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReaderUsersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReaderUsersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dyatkokg/bookreaderusersapi/BookReaderUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.bookreaderusersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReaderUsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
