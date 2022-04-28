64bb2ef6228d7f2f2ccb7f38557454ba8f70a675
dyatkokg <dyatkokg@gmail.com>
2022-04-28 15:47:53 +0400

    transfer token signature to cloud config app

--- src/main/java/me/dyatkokg/bookreaderusersapi/service/implementation/TokenProviderImplementation.java
+++ src/main/java/me/dyatkokg/bookreaderusersapi/service/implementation/TokenProviderImplementation.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import lombok.RequiredArgsConstructor;
 import me.dyatkokg.bookreaderusersapi.entity.User;
 import me.dyatkokg.bookreaderusersapi.service.TokenProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -14,7 +15,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class TokenProviderImplementation implements TokenProvider {
 
-    private static final String SIGNATURE = "x7HktL7QmQwR7Ii98AU3eACZ70arilFhCIgz54IYEOMCL0UIl7MWCBXeEzyQ6syQmWXWJACQzOWkrWd2DBnibCbENjd6fV7m073e27KkERwBSrmSX0hv28P8rP6LSXY1xs24Fr6w3Se7amJ9fRSPfIL0cXdujrtUUPjPp8M1M6SKJezzOMkSKZtihTKRscQrBjcTPJWs";
+    @Value("${token.signature}")
+    private String signature;
 
     public String generateToken(User user) {
         Date currentDate = new Date();
@@ -24,9 +26,9 @@ public class TokenProviderImplementation implements TokenProvider {
                 .withIssuedAt(currentDate)
                 .withExpiresAt(expireAt)
                 .withIssuer("book-app")
-                .withSubject(user.getEmail())
+                .withSubject(user.getId())
                 .withJWTId(UUID.randomUUID().toString())
-                .sign(Algorithm.HMAC256(SIGNATURE));
+                .sign(Algorithm.HMAC256(signature));
     }
 
 }
